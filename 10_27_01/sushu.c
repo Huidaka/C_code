fc04f7473e6ee5e064a27ce4f7e5e0cc6bfe1c03
Huidaka <2585275370@qq.com>
2019-10-31 19:58:19 +0800

    添加了素数判断函数

--- 10_27_01/sushu.c
+++ 10_27_01/sushu.c
@@ -1,20 +1,39 @@
-#include <stdio.h>
-int main()
-{
-	int i, j;
-	printf("100到200之间的素数：\n");
-	for (i = 100; i <= 200; i++) {
-		for (j = 2; j < i; j++) 
+//#include <stdio.h>
+//int main()
+//{
+//	int i, j;
+//	printf("100到200之间的素数：\n");
+//	for (i = 100; i <= 200; i++) {
+//		for (j = 2; j < i; j++) 
+//		{
+//			if (i % j == 0)
+//			{
+//				break;
+//			}
+//		}
+//		if (i == j)
+//		{
+//			printf("%d\n", i);
+//		}
+//	}
+//	return 0;
+//}
+#include<stdio.h>
+int Sushu(int x) {
+	int i;
+	for (i = 2; i < x; i++)
+	{
+		if (x % i == 0)
 		{
-			if (i % j == 0)
-			{
-				break;
-			}
-		}
-		if (i == j)
-		{
-			printf("%d\n", i);
+			return 0;
 		}
 	}
-	return 0;
+	return 1;
+}
+int main() {
+	int i;
+	for (i = 100; i <= 200; i++) {
+		if (Sushu(i))
+			printf("%d\n", i);
+	}
 }
