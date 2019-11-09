31aa2911ec8aa80ba39e0029726b118923aad5ec
Huidaka <2585275370@qq.com>
2019-11-09 17:32:33 +0800

    优化了mystrlen的健壮性

--- 11_09_02/11_09_02.c
+++ 11_09_02/11_09_02.c
@@ -1,13 +1,15 @@
 #define  _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
-int mystrlen(char* str)
+#include<assert.h>
+int mystrlen(const char* str)
 {
+	assert(str != NULL);
 	char* p = str; 
-	while (*str)
+	while (*str++)
 	{
-		str++;
+
 	 }
-	return str - p;
+	return str - p-1;
 }
 int main()
 {
