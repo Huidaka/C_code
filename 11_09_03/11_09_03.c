6fef28293a75ee110f1cdc0e15f06c3c5c0c91fc
Huidaka <2585275370@qq.com>
2019-11-10 23:40:33 +0800

    模拟实现strcpy函数

---
+++ 11_09_03/11_09_03.c
@@ -0,0 +1,31 @@
+#define  _CRT_SECURE_NO_WARNINGS 
+#include<stdio.h>
+#include<assert.h>
+#include<string.h>
+//char* my_strcpy(char* Str_treget, const char* Str_souce)
+//{
+//	char* ret = Str_treget;
+//	assert(Str_treget != NULL);
+//	assert(Str_souce != NULL);
+//	while (*Str_treget++ = *Str_souce++)
+//	{
+//
+//	}
+//	return ret;
+//}
+//int main()
+//{
+//	char arr1[] = "hahaha";
+//	char arr2[10];
+//	my_strcpy(arr2, arr1);
+//	printf("%s", arr2);
+//	return 0;
+//}
+int main()
+{
+	unsigned int a = 3;
+	unsigned int b = 6;
+	printf("%u",a-b);
+	strcpy();
+	return 0;
+}
