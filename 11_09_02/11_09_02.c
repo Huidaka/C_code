1ec27f026699280e5beb1dd6b1f8a3fd607ee438
Huidaka <2585275370@qq.com>
2019-11-12 23:39:24 +0800

    递归和非递归实现my_strlen

--- 11_09_02/11_09_02.c
+++ 11_09_02/11_09_02.c
@@ -1,19 +1,34 @@
 #define  _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<assert.h>
-int mystrlen(const char* str)
+//int mystrlen(const char* str)
+//{
+//	assert(str != NULL);
+//	char* p = str; 
+//	while (*str++)
+//	{
+//
+//	 }
+//	return  str-p-1;
+//}
+//int main()
+//{
+//	char str[] =  "gfdgsfsfs";
+//	printf("%d\n", mystrlen(str));
+//	return 0;
+//}
+int mystrlen(char* str)
 {
 	assert(str != NULL);
-	char* p = str; 
-	while (*str++)
+	if (*str == '\0')
 	{
-
-	 }
-	return str - p-1;
+		return 0;
+	}
+	return 1+ mystrlen(++str);
 }
 int main()
 {
-	char str[] =  "gfdgsfsfs";
+	char str[] = "Huidaka";
 	printf("%d\n", mystrlen(str));
 	return 0;
 }
